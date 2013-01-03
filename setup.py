b69624431f03acd10a0f02957faa70cb582b8a6b
Christian Ternus <ternus@cternus.net>
2013-01-03 13:10:47 -0500

    new setup version

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='gametex-django',
-    version='0.2.1',
-    packages=['gametex','gametex.management.commands'],
+    version='0.3.0',
+    packages=find_packages(),
     author='Christian Ternus',
     author_email='ternus@cternus.net',
     url='http://github.com/ternus/gametex-django',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='Django integration for GameTeX',
     long_description=open('README.md').read(),
+    install_requires=['Django'],
 )
